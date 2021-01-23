fae7bc3f7f230dbc24006869ee90bff393fb1876
DesertSkate <robertschaz@gmail.com>
2021-01-23 16:34:38 -0700

    Init

---
+++ main.py
@@ -0,0 +1,156 @@
+import pygame
+import math
+import random
+from templates import Templates
+
+pygame.init()
+size = width, height = 1200, 800
+window = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+pygame.display.set_caption("Conway's Game of Life")
+playing = True
+started = False
+cell_horizontal = 100
+cell_vertical = 100
+
+
+def gen_grid(width, height):
+    arr = []
+    for i in range(height):
+        arr.append([0 for i in range(width)])
+    return arr
+
+
+map_array = gen_grid(cell_horizontal, cell_vertical)
+templates = Templates()
+
+
+def draw_grid():
+    for i in range(len(map_array)):
+        pygame.draw.line(window, (100,100,100), (0, (height / len(map_array) * i)), (width, (height / len(map_array) * i)))
+        for j in range(len(map_array[i])):
+            pygame.draw.line(window, (100,100,100), ((width / len(map_array[i]) * j), 0), ((width / len(map_array[i])) * j, height))
+
+
+def draw_cells():
+    for i in range(len(map_array)):
+        for j in range(len(map_array[i])):
+            cords = ((width / len(map_array[i])) * j, (height / len(map_array)) * i)
+            size = (width / len(map_array[i]), height / len(map_array))
+            rect = pygame.Rect(cords, size)
+            if map_array[i][j] == 1:
+                pygame.draw.rect(window, (155, 155, 0), rect)
+            elif map_array[i][j] == 2:
+                pygame.draw.rect(window, (0, 255, 0), rect)
+
+
+def get_neighbours(x, y):
+    # TODO optimize this
+    neighbours = []
+    for i in range(-1, 2):
+        next_y = y + i
+        if not next_y >= 0:
+            next_y = len(map_array) - 1
+        elif next_y > len(map_array) - 1:
+            next_y = 0
+        for j in range(-1, 2):
+            next_x = x + j
+            if not next_x >= 0:
+                next_x = len(map_array[i]) - 1
+            elif next_x > len(map_array) - 1:
+                next_x = 0
+
+            if i == 0 and j == 0:
+                continue
+
+            if map_array[next_y][next_x] == 1:
+                neighbours.append([next_y, next_x])
+
+    # if len(neighbours) > 0:
+    #     # print(f"{neighbours} for cell x:{x} y:{y}")
+    #     if map_array[y][x] != 1:
+    #         map_array[y][x] = 2
+    #         # print(next_y, next_x)
+    return len(neighbours)
+
+
+def pos_to_cell(x, y):
+    cell_width = width / len(map_array[0])
+    cell_height = height / len(map_array)
+    index = (math.floor(x / cell_width), math.floor(y / cell_height))
+    return index
+
+
+def create_random():
+    global map_array
+    map_array = gen_grid(cell_horizontal, cell_vertical)
+    for i in range(len(map_array)):
+        for j in range(len(map_array[i])):
+            map_array[i][j] = random.choice([0, 1])
+
+
+def simulate_cells():
+    to_kill = []
+    to_live = []
+    for i in range(len(map_array)):
+
+        for j in range(len(map_array[i])):
+
+            neighbours = get_neighbours(j, i)
+            # print(neighbours)
+            # live = sum([x.count(1) for x in neighbours]) - 1 # Accounting for self
+            # if neighbours > 0 and map_array[j][i] == 1:
+            #     # print(live)
+            #     print(neighbours)
+            # print(live)
+
+            if map_array[i][j] == 0:
+                if neighbours == 3:
+                    to_live.append((i,j))
+                    # print(f"Save {i} {j}")
+
+            if map_array[i][j] == 1:
+                if neighbours == 2 or neighbours == 3:
+                    # print("test")
+                    continue
+                else:
+                    # print(f"Kill {i} {j}")
+                    to_kill.append((i,j))
+    for i in to_kill:
+        map_array[i[0]][i[1]] = 0
+    for i in to_live:
+        map_array[i[0]][i[1]] = 1
+
+    # map_array = arr
+
+
+while playing:
+    window.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                started = True if not started else False
+            if event.key == pygame.K_s:
+                started = False
+                map_array = gen_grid(cell_horizontal, cell_vertical)
+            if event.key == pygame.K_r:
+                started = False
+                create_random()
+            if event.key == pygame.K_1:
+                started = False
+                map_array = templates.create_methuselahs(random.choice([1,2,3]))
+            if event.key == pygame.K_2:
+                started = False
+                map_array = templates.create_gosper_gun()
+
+    if pygame.mouse.get_pressed()[0]:
+        pos = pos_to_cell(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+        map_array[pos[1]][pos[0]] = 1
+    if started:
+        simulate_cells()
+    draw_grid()
+    draw_cells()
+    clock.tick(10)
+    pygame.display.update()
---
+++ templates.py
@@ -0,0 +1,67 @@
+class Templates:
+    def __init__(self):
+        pass
+
+    def gen_grid(self, width, height):
+        arr = []
+        for i in range(height):
+            arr.append([0 for i in range(width)])
+        return arr
+
+    def create_methuselahs(self, type):
+        # Make it so don't have to generate for each one
+        self.map_array = self.gen_grid(100, 100)
+        if type == 1:
+            self.map_array[49][49] = 1
+            self.map_array[48][48:50] = [1] * 2
+            self.map_array[47][49:51] = [1] * 2
+        elif type == 2:
+            self.map_array[49][42] = 1
+            print(len(self.map_array))
+            self.map_array[49][46:49] = [1] * 3
+            print(len(self.map_array[49]))
+            self.map_array[48][41:43] = [1] * 2
+            print(len(self.map_array))
+            self.map_array[47][47] = 1
+        elif type == 3:
+            self.map_array[49][43:45] = [1] * 2
+            self.map_array[49][47:50] = [1] * 3
+            self.map_array[48][46] = 1
+            self.map_array[47][44] = 1
+        return self.map_array
+
+    def create_gosper_gun(self):
+        # 14 - 50, x
+        # 41 - 50, y
+        self.map_array = self.gen_grid(100, 100)
+        self.map_array[50][13:15] = [1] * 2
+        self.map_array[49][12] = 1
+        self.map_array[49][16] = 1
+        self.map_array[48][11] = 1
+        self.map_array[48][17] = 1
+        self.map_array[48][25] = 1
+        self.map_array[47][1:3] = [1] * 2
+        self.map_array[47][11] = 1
+        self.map_array[47][15] = 1
+        self.map_array[47][17:19] = [1] * 2
+        self.map_array[47][23] = 1
+        self.map_array[47][25] = 1
+        self.map_array[46][1:3] = [1] * 2
+        self.map_array[46][17] = [1] * 2
+        self.map_array[46][11] = 1
+        self.map_array[46][17] = 1
+        self.map_array[46][21:23] = [1] * 2
+        self.map_array[45][12] = 1
+        self.map_array[45][16] = 1
+        self.map_array[45][21:23] = [1] * 2
+        self.map_array[45][35:37] = [1] * 2
+        self.map_array[44][13:15] = [1] * 2
+        self.map_array[44][21:23] = [1] * 2
+        self.map_array[44][35:37] = [1] * 2
+        self.map_array[43][23] = 1
+        self.map_array[43][25] = 1
+        self.map_array[42][25] = 1
+
+
+
+        return self.map_array
